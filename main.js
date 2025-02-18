07abe16242ceab2e0231d2c783ef1efd88572493
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-18 15:26:33 +0100

    add two images add function turn on add button

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 function turnOn(btn) {
-    
+    if (btn) {
+        document.getElementById('yellow').style.display = "block";
+        document.getElementById('white').style.display = "none";
+
+    } else {
+        document.getElementById('yellow').style.display = "none";
+        document.getElementById('white').style.display = "block";
+
+    }
 }
 
-const turn_on = document.addEventListener('click',)
+const turn_on = document.addEventListener('click', turnOn());
+
